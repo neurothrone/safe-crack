dc99d5b0f6e4757b77b5e44cb3f38be1ca000987
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-12 20:40:43 +0200

    Initial Commit

---
+++ SafeCrack Watch App/SafeCrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeCrackApp.swift
+//  SafeCrack Watch App
+//
+//  Created by Zaid Neurothrone on 2022-10-12.
+//
+
+import SwiftUI
+
+@main
+struct SafeCrack_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
